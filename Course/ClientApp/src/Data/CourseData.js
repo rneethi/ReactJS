3d5fc4e53069dadc5e0b309694c6423b5c45a5dc
Ramachandran Neethi <ramachandran.neethi@gmail.com>
2020-04-21 11:59:45 +0530

    Courses - Logically add the layout without repeating

---
+++ Course/ClientApp/src/Data/CourseData.js
@@ -0,0 +1,38 @@
+﻿
+import Default from '../Img/Image.jpg';
+import Angular from '../Img/Angular.svg';
+import Devops from '../Img/Devops.jpg';
+import PM from '../Img/PM.jpg';
+import SoftwareTesting from '../Img/SoftwareTesting.png';
+
+export const courseData = [
+    {
+        Id: 1,
+        courseName: "C# Programming ",
+        image: Default
+    },
+    {
+        Id: 2,
+        courseName: "Angular Freamwork",
+        image: Angular
+    },
+    {
+        Id: 3,
+        courseName: "DevOps",
+        image: Devops
+    },
+    {
+        Id: 4,
+        courseName: "Software Testing",
+        image: SoftwareTesting
+    },
+    {
+        Id: 5,
+        courseName: "Project Management",
+        image: PM
+    }
+
+
+]
+
+export default courseData;
